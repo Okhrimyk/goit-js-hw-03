aa1708ee5fab845c344703b2f94bf6b5551d2b29
Val <valeriiokhrimyk@gmail.com>
2025-09-01 16:20:51 -0300

    home work

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,14 @@
-function checkForSpam(message) {
-    message = message.toLowerCase();
-    const isHasSpam = message.includes("sale") || message.includes("spam");
-    return isHasSpam ? true : false;
-}
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+function filterArray(numbers, value) {
+    let newArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];            
+        if (element > value) {
+            newArray.push(element);
+        }
+    } return newArray;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
